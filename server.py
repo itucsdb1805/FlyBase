c641c6bfc36ea93426aa54035435f8b997b96ffd
K Bulut Ozler <kbulutozler@protonmail.com>
2018-10-22 23:06:20 +0300

    redo what i did
     'appp' to 'app'

--- server.py
+++ server.py
@@ -6,23 +6,23 @@ from movie import Movie
 
 
 def create_app():
-    appp = Flask(__name__)
-    appp.config.from_object("settings")
+    app = Flask(__name__)
+    app.config.from_object("settings")
 
-    appp.add_url_rule("/", view_func=views.home_page)
-    appp.add_url_rule("/movies", view_func=views.movies_page)
-    appp.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
+    app.add_url_rule("/", view_func=views.home_page)
+    app.add_url_rule("/movies", view_func=views.movies_page)
+    app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
 
 
     db = Database()
     db.add_movie(Movie("Slaughterhouse-Five", year=1972))
     db.add_movie(Movie("The Shining"))
-    appp.config["db"] = db
+    app.config["db"] = db
 
-    return appp
+    return app
 
 
 if __name__ == "__main__":
-    appp = create_app()
-    port = appp.config.get("PORT", 5000)
-    appp.run(host="0.0.0.0", port=port)
+    app = create_app()
+    port = app.config.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
