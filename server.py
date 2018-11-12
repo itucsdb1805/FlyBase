bfb55e00b69f473193e75da4464c9e3dc5962a65
bulut <ozler16@itu.edu.tr>
2018-11-12 23:14:24 +0300

    app initialization out of main

--- server.py
+++ server.py
@@ -13,7 +13,6 @@ def create_app():
     app.add_url_rule("/movies", view_func=views.movies_page)
     app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
 
-
     db = Database()
     db.add_movie(Movie("Slaughterhouse-Five", year=1972))
     db.add_movie(Movie("The Shining"))
@@ -22,7 +21,8 @@ def create_app():
     return app
 
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     port = app.config.get("PORT", 5000)
     app.run(host="0.0.0.0", port=port)
