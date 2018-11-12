55e138aa579265ee3ac3e1acead8121b48f11a94
abgulhan <44143449+abgulhan@users.noreply.github.com>
2018-11-12 23:47:50 +0300

    Add sql table initializtion code to dbinit.py
    This commit modifies the variable INIT_STATEMENTS in the file dbinit.py.  The following tables were added to the sql server:
    COUNTRIES, PASSENGERS, AIRLINE_WORKERS, FLIGHTS, WORKERS_PER_FLIGHT

--- dbinit.py
+++ dbinit.py
@@ -5,8 +5,63 @@ import psycopg2 as dbapi2
 
 
 INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
+""" CREATE TABLE IF NOT EXISTS COUNTRIES(
+        country_id serial PRIMARY KEY,
+        country_name varchar(50) NOT NULL
+        );""",
+
+""" CREATE TABLE IF NOT EXISTS PASSENGERS(
+        passenger_id serial PRIMARY KEY,
+        name varchar (20) NOT NULL,
+        middle_name varchar (20) DEFAULT NULL,
+        surname varchar (20) NOT NULL,
+        gender  varchar (1) NOT NULL
+            CHECK (gender IN ( 'F' , 'M' ) ),
+        birth_date date NOT NULL,
+        country_id integer NOT NULL,
+        flight_count integer DEFAULT 0,
+        last_flight_day date DEFAULT NULL,
+        FOREIGN KEY (country_id) REFERENCES COUNTRIES(country_id) ON DELETE RESTRICT
+        );""",
+
+""" CREATE TABLE IF NOT EXISTS AIRLINE_WORKERS(
+        worker_id serial PRIMARY KEY,
+        name varchar (20) NOT NULL,
+        middle_name varchar (20) DEFAULT NULL,
+        surname varchar (20) NOT NULL,
+        gender  varchar (1) NOT NULL
+            CHECK (gender IN ( 'F' , 'M' ) ),
+        birth_date date NOT NULL,
+        country_id integer NOT NULL ,
+        flight_hours integer DEFAULT 0,
+        --airline_id integer NOT NULL REFERENCES AIRLINES(airline_ID) ON DELETE RESTRICT,
+        job  varchar(15) NOT NULL
+            CHECK (job IN ( 'pilot' , 'hostess' , 'other' ) ),
+        photo varchar(50) DEFAULT './photos/default.jpg',
+        FOREIGN KEY (country_id) REFERENCES COUNTRIES(country_id) ON DELETE RESTRICT
+        );""",
+
+""" CREATE TABLE IF NOT EXISTS FLIGHTS(
+        flight_id serial PRIMARY KEY,
+        --airline_id integer NOT NULL REFERENCES AIRLINES(airline_id) ON DELETE RESTRICT,
+        --aircraft_id integer NOT NULL REFERENCES AIRLINES(aircraft_id) ON DELETE RESTRICT,
+        --route_id integer NOT NULL REFERENCES AIRLINES(route_id) ON DELETE RESTRICT,
+        departure timestamp,
+        arrival timestamp,
+        passenger_count integer DEFAULT 0,
+        fuel_required decimal DEFAULT 0.0
+        );"""
+
+""" CREATE TABLE IF NOT EXISTS WORKERS_PER_FLIGHT(
+        flight_id integer NOT NULL,
+        worker_id integer NOT NULL,
+        ord integer DEFAULT 0,
+            CHECK (ord > 0),
+        FOREIGN KEY (flight_id) REFERENCES FLIGHTS(flight_id),
+        FOREIGN KEY (worker_id) REFERENCES AIRLINE_WORKERS(worker_id),
+        PRIMARY KEY (flight_id,worker_id)
+        );"""
+        
 ]
 
 
@@ -23,4 +78,4 @@ if __name__ == "__main__":
     if url is None:
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
         sys.exit(1)
-    initialize(url)
+initialize(url)
