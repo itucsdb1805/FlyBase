a3b136a838bddf63c9aa36ae399c2f8d7cdb2356
abgulhan <44143449+abgulhan@users.noreply.github.com>
2018-11-13 11:50:23 +0300

    Add page to where user can add items to database
    The page add_country was added.  In this page the user can add a country to the database using a form.  The page then redirects to another page show the list of all countries and their respective ID's.

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import request
+from flask import redirect
+from flask import url_for
 
 import views
 from database import Database
@@ -12,6 +15,10 @@ def create_app():
     app.add_url_rule("/", view_func=views.home_page)
     app.add_url_rule("/flights", view_func=views.flights_page)
     app.add_url_rule("/flights/<int:flight_key>", view_func=views.flight_page)
+    app.add_url_rule(
+        "/add_country", view_func=views.add_page, methods=["GET", "POST"]
+    )
+    app.add_url_rule("/countries", view_func=views.countries_page)
 
     db = Database()
     db.add_flight(Flight("IST-ESB", date="10-10-2018", airport="IST"))
--- views.py
+++ views.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 from flask import abort, current_app, render_template
+from flask import request
+from flask import redirect
+from flask import url_for
 
 def home_page():
     today = datetime.today()
@@ -17,3 +20,47 @@ def flight_page(flight_key):
     if flight is None:
         abort(404)
     return render_template("flight.html", flight=flight)
+
+
+
+def add_page():
+    statement = """INSERT INTO COUNTRIES (country_name)
+        VALUES (%(name)s)"""
+
+
+    if request.method == "GET":
+        return render_template("add_country.html")
+    else:
+        form_name = request.form["name"]
+        
+        try:
+            connection = dbapi2.connect(dsn)
+            cursor = connection.cursor()
+            if (form_name):
+                cursor.execute(statement, {'name': country_name})
+            cursor.close()
+            connection.close()
+        except dbapi2.DatabaseError:
+            connection.rollback()
+        finally:
+            return redirect(url_for("countries_page"))
+
+def countries_page():
+    statement = """SELECT country_id, country_name
+        FROM COUNTRIES)"""
+    data = ""
+    try:
+        connection = dbapi2.connect(dsn)
+        cursor = connection.cursor()
+        cursor.execute(statement)
+        data = curser.fetchall()
+        for country_id, country_name in cursor:
+            print('%(id)s: %(nm)s' % {'id': country_id, 'nm': country_name})
+        cursor.close()
+        connection.close()
+    except dbapi2.DatabaseError:
+        connection.rollback()
+    finally:
+  
+        return render_template("countries.html", data=sorted(data))
+    
