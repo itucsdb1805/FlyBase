c2c7b76d3e6e9640aa61e778605c46c3a7e53714
K Bulut Ozler <ozler16@itu.edu.tr>
2018-11-27 18:56:27 +0300

    URL settings
    url variable is changed to fix database connection issue
    'form_name' is added to line 46
    connection.commit() commands are added

--- views.py
+++ views.py
@@ -1,8 +1,13 @@
+import os
 from datetime import datetime
+from sqlite3 import dbapi2
+
 from flask import abort, current_app, render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+from psycopg2.tests import dsn
+
 
 def home_page():
     today = datetime.today()
@@ -32,56 +37,51 @@ def add_page():
         return render_template("add_country.html")
     else:
         form_name = request.form["name"]
-        
+        print(form_name)
         try:
-            connection = dbapi2.connect(dsn)
+            url = "postgres://itucs:itucspw@localhost:32768/itucsdb"
+            connection = dbapi2.connect(url)
             cursor = connection.cursor()
             if (form_name):
-                cursor.execute(statement, {'name': country_name})
+                cursor.execute(statement, {'name': form_name})
+
+                print("Execute works!")
+                connection.commit()
+
             cursor.close()
             connection.close()
         except dbapi2.DatabaseError:
+            print("dataerror1")
+            print(dbapi2.DatabaseError)
             connection.rollback()
         finally:
             return redirect(url_for("countries_page"))
 
 def countries_page():
+
     statement = """SELECT country_id, country_name
-        FROM COUNTRIES)"""
+        FROM COUNTRIES"""
     data = ""
     try:
-        connection = dbapi2.connect(dsn)
+
+        url = "postgres://itucs:itucspw@localhost:32768/itucsdb"
+        connection = dbapi2.connect(url)
         cursor = connection.cursor()
+
         cursor.execute(statement)
+        print("Execute works!")
+
         data = cursor.fetchall()
+        print(data)
         for country_id, country_name in cursor:
             print('%(id)s: %(nm)s' % {'id': country_id, 'nm': country_name})
         cursor.close()
         connection.close()
+
     except dbapi2.DatabaseError:
+        print("dataError2")
         connection.rollback()
+
     finally:
-  
         return render_template("countries.html", data=sorted(data))
-
-
-# def passenger_add_page():
-    # if request.method == "GET":
-        # return render_template("passenger_adding.html")
-
-    # else:
-        # form_name = request.form["name"]
-        # form_surname = request.form["surname"]
-        # form_country = request.form["country"]
-        # form_phone = request.form["phone"]
-        # form_airline = request.form["airline"]
-        # form_departure = request.form["departure"]
-        # form_arrival = request.form["arrival"]
-        # form_date = request.form["date"]
-        # form_payment = request.form["payment"]
-
-        
-        # db = current_app.config["db"]
-        
-        # return redirect(url_for("", ))
     
