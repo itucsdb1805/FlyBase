111273e2ffb7a6402ff5f7a49317cea58ce0fc91
abgulhan <44143449+abgulhan@users.noreply.github.com>
2018-11-28 12:24:35 +0300

    Change dabatabe url address is views.py
    The database url was showing the local url, this was changed to the heroku url.

--- views.py
+++ views.py
@@ -39,7 +39,7 @@ def add_page():
         form_name = request.form["name"]
 
         try:
-            url = "postgres://itucs:itucspw@localhost:32769/itucsdb"
+            url =  os.getenv("DATABASE_URL")  #"postgres://itucs:itucspw@localhost:32769/itucsdb"
             connection = dbapi2.connect(url)
             cursor = connection.cursor()
             if (form_name):
@@ -64,7 +64,7 @@ def countries_page():
     data = ""
     try:
 
-        url = "postgres://itucs:itucspw@localhost:32769/itucsdb"
+        url =  os.getenv("DATABASE_URL")  #"postgres://itucs:itucspw@localhost:32769/itucsdb"
         connection = dbapi2.connect(url)
         cursor = connection.cursor()
 
